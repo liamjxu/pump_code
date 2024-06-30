d0aaad0e4a0e52947054d9110210cff5a5538bcc
Jialiang Xu <xjl@stanford.edu>
2024-06-30 14:40:31 -0700

    update the llm

--- Persona-DB/llm_api.py
+++ Persona-DB/llm_api.py
@@ -10,30 +10,40 @@ def chatbot(input, model="bedrock-runtime", response_format="text", batch_size=1
 
     output=None
     if "gpt" in model:
-
         while True:
             try:
-
                 body = json.dumps({
-                    "prompt": f"\n\nHuman: {input}\n\nAssistant:",
-                    "max_tokens_to_sample": 300,
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "max_tokens": 1000,
                     "temperature": 0,
-                    "top_p": 0.9,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "text",
+                                    "text": input,
+                                }
+                            ]
+                        }
+                    ]
                 })
 
+                inputs = {
+                    "modelId": "anthropic.claude-3-sonnet-20240229-v1:0",
+                    "contentType": "application/json",
+                    "accept": "application/json",
+                    "body": body
+                }
 
-                modelId = 'anthropic.claude-v2'
-                accept = 'application/json'
-                contentType = 'application/json'
-
-                response = brt.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+                response = brt.invoke_model(**inputs)
 
                 response_body = json.loads(response.get('body').read())
 
                 # text
-                output = response_body.get('completion')
-
+                output = response_body.get('content')[0]['text']
                 break
+
             except Exception as e:  # wait 1 second and try again
                 print(e)
                 if e.type == "invalid_request_error":
