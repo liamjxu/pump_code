ed1044be4ed75578d8333c31009f753c919f8c1b
Jialiang Xu <xjl@stanford.edu>
2024-07-08 22:57:00 +0000

    util, get files

--- pump/utils.py
+++ pump/utils.py
@@ -1,9 +1,14 @@
 import boto3
 import json
+from io import StringIO, BytesIO
+
 
 brt = boto3.client(service_name='bedrock-runtime')
+s3_client = boto3.client('s3')
+bucket_name = 'probabilistic-user-modeling'
 
-def get_claude(input_text):
+
+def get_llm_response(input_text):
     body = json.dumps({
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": 1000,
@@ -35,3 +40,18 @@ def get_claude(input_text):
     # text
     output = response_body.get('content')[0]['text']
     return output
+
+
+def get_file(file_key='info.csv'):
+    if file_key.endswith('.npy'):
+        s3_object = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+        s3_data = s3_object['Body'].read()
+        return BytesIO(s3_data)
+
+    elif file_key.endswith('.csv'):
+        s3_object = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+        s3_data = s3_object['Body'].read().decode('utf-8')
+        return StringIO(s3_data)
+
+    else:
+        raise Exception('unknown file type')
