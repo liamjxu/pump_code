4d3b6c31fd3146ca644a1d299a7b7ce2e6059f4d
Jialiang Xu <xjl@stanford.edu>
2024-07-10 05:13:49 +0000

    initial merging

--- pump/utils.py
+++ pump/utils.py
@@ -1,6 +1,9 @@
 import boto3
 import json
+import torch
+import torch.nn.functional as F
 from io import StringIO, BytesIO
+from torch import Tensor
 
 
 brt = boto3.client(service_name='bedrock-runtime')
@@ -98,3 +101,20 @@ def get_topics(mapping, question):
         if key.startswith(question):
             return mapping[key]
     return None
+
+
+def last_token_pool(last_hidden_states: Tensor,
+                 attention_mask: Tensor) -> Tensor:
+    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+    if left_padding:
+        return last_hidden_states[:, -1]
+    else:
+        sequence_lengths = attention_mask.sum(dim=1) - 1
+        batch_size = last_hidden_states.shape[0]
+        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+
+
+def get_detailed_instruct(task_description: str, query: str) -> str:
+    return f'Instruct: {task_description}\n{query}'
+
+
