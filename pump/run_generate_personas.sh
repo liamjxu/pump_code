d2a2a9305301af5492f65ec7b6ede0709ce0eaca
Jialiang Xu <xjl@stanford.edu>
2024-07-15 16:19:31 -0700

    minor

--- pump/run_generate_personas.sh
+++ pump/run_generate_personas.sh
@@ -1,6 +1,7 @@
-python -m generate_personas --output_dir_root sm_local/outputs_sonnet_kmeans_single_example \
-                            --model_id anthropic.claude-3-sonnet-20240229-v1:0 \
-                            --extraction_prompt_type example \
-                            --clustering_algo kmeans \
-                            --clustering_num_clusters 20 \
-                            --merging_personas_from_surveys single \
+python -m generate_personas \
+    --output_dir_root sm_local/outputs_sonnet_kmeans_single_example \
+    --model_id anthropic.claude-3-sonnet-20240229-v1:0 \
+    --extraction_prompt_type example \
+    --clustering_algo kmeans \
+    --clustering_num_clusters 20 \
+    --merging_personas_from_surveys single
