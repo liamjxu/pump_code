03078ab66eed6eedac330a24fe06ef048104bb8b
Jialiang Xu <xjl@stanford.edu>
2024-07-15 16:13:31 -0700

    refactor

---
+++ run_generate_personas.sh
@@ -0,0 +1,7 @@
+python generate_personas.py \
+    --output_dir_root sm_local/outputs_sonnet_kmeans_single_example \
+    --model_id anthropic.claude-3-sonnet-20240229-v1:0 \
+    --extraction_prompt_type example \
+    --clustering_algo kmeans \
+    --clustering_num_clusters 20 \
+    --merging_personas_from_surveys single \
