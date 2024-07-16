00aa876ca398c16c28732f321964aceba59beddc
Jialiang Xu <xjl@stanford.edu>
2024-07-15 22:28:26 -0700

    haiku

---
+++ pump/scripts/run_generate_personas_haiku.sh
@@ -0,0 +1,8 @@
+python -m generate_personas \
+    --output_dir_root sm_local/outputs_sonnet_kmeans_single_example \
+    --model_id anthropic.claude-3-haiku-20240307-v1:0 \
+    --extraction_prompt_type example \
+    --clustering_algo kmeans \
+    --clustering_num_clusters 20 \
+    --merging_personas_from_surveys single \
+    --survey_ending 5
