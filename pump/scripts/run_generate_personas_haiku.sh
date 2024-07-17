840ac03e0cf649bb07e8550f7e17f8195f899939
Jialiang Xu <xjl@stanford.edu>
2024-07-17 09:09:51 -0700

    don't skip

--- pump/scripts/run_generate_personas_haiku.sh
+++ pump/scripts/run_generate_personas_haiku.sh
@@ -1,6 +1,5 @@
 python -m generate_personas \
     --debug \
-    --skip_existing \
     --output_dir_root sm_local/outputs_haiku_kmeans_single_example \
     --model_id anthropic.claude-3-haiku-20240307-v1:0 \
     --extraction_prompt_type example \
--- pump/scripts/run_generate_personas_sonnet_kmeans10.sh
+++ pump/scripts/run_generate_personas_sonnet_kmeans10.sh
@@ -1,6 +1,5 @@
 python -m generate_personas \
     --debug \
-    --skip_existing \
     --output_dir_root sm_local/outputs_sonnet_kmeans10_single_example \
     --model_id anthropic.claude-3-sonnet-20240229-v1:0 \
     --extraction_prompt_type example \
