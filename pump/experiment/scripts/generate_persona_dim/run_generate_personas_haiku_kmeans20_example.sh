75a3e9571ea8067c99a62074ed879eadde1107b6
liamjxu <xjl@stanford.edu>
2024-08-28 22:46:37 +0000

    script

--- pump/experiment/scripts/generate_persona_dim/run_generate_personas_haiku_kmeans20_example.sh
+++ pump/experiment/scripts/generate_persona_dim/run_generate_personas_haiku_kmeans20_example.sh
@@ -1,8 +1,8 @@
 # Generate with haiku
 python -m src.generate_personas \
     --debug \
-    --output_dir_root opinions_qa/persona_dim/haiku_kmeans10_single_example \
-    --model_id anthropic.claude-3-haiku-20240307-v1:0 \
+    --output_dir_root opinions_qa/persona_dim/date0828_haiku_kmeans20_single_example \
+    --model_id haiku \
     --extraction_prompt_type example \
     --clustering_algo kmeans \
     --clustering_num_clusters 20 \
